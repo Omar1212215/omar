a2b02a8aba7700bb94c650717590d07d7c218c9b
Omar1212215 <omarycosherif@gmail.com>
2025-12-22 14:42:49 +0200

    1

--- Assets/EnemyMovemnt.cs
+++ Assets/EnemyMovemnt.cs
@@ -5,14 +5,14 @@ public class EnemyMovemnt : MonoBehaviour
 {
     public Transform player;
     private NavMeshAgent navMeshAgent;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
 
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
          if (player != null)
